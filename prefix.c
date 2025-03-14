00324c9d9d83519c21bd8e54a4a9a33f8067c629
Prakul C <prakulc6@gmail.com>
2025-03-14 18:09:28 +0530

    Create prefix.c

---
+++ prefix.c
@@ -0,0 +1,203 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+#define MAX 30
+
+struct Stack {
+    char items[MAX];
+    int top;
+};
+
+struct EvalStack {
+    int items[MAX];
+    int top;
+};
+
+void initStack(struct Stack *s) {
+    s->top = -1;
+}
+
+void initEvalStack(struct EvalStack *s) {
+    s->top = -1;
+}
+
+int isEmpty(struct Stack *s) {
+    return s->top == -1;
+}
+
+int isEvalEmpty(struct EvalStack *s) {
+    return s->top == -1;
+}
+
+void push(struct Stack *s, char value) {
+    if (s->top == MAX - 1) {
+        printf("Stack Overflow\n");
+    } else {
+        s->items[++(s->top)] = value;
+    }
+}
+
+void evalPush(struct EvalStack *s, int value) {
+    if (s->top == MAX - 1) {
+        printf("Stack Overflow\n");
+    } else {
+        s->items[++(s->top)] = value;
+    }
+}
+
+char pop(struct Stack *s) {
+    if (isEmpty(s)) {
+        printf("Stack Underflow\n");
+        return '#';
+    } else {
+        return s->items[(s->top)--];
+    }
+}
+
+int evalPop(struct EvalStack *s) {
+    if (isEvalEmpty(s)) {
+        printf("Stack Underflow\n");
+        return -1;
+    } else {
+        return s->items[(s->top)--];
+    }
+}
+
+// Function to check Stack Precedence
+int stackprec(char symbol) {
+    switch (symbol) {
+        case '+':
+        case '-': return 1;
+        case '*':
+        case '/':
+        case '%': return 3;
+        case '^':
+        case '$': return 6;
+        case ')': return 0;
+        case '#': return -1;
+        default: return 8;
+    }
+}
+
+// Function to check Input Precedence
+int inprec(char symbol) {
+    switch (symbol) {
+        case '+':
+        case '-': return 2;
+        case '*':
+        case '/':
+        case '%': return 4;
+        case '^':
+        case '$': return 5;
+        case '(': return 0;
+        case ')': return 9;
+        default: return 7;
+    }
+}
+
+// Function to reverse the string
+void reverseString(char str[]) {
+    int length = strlen(str);
+    for (int i = 0; i < length / 2; i++) {
+        char temp = str[i];
+        str[i] = str[length - 1 - i];
+        str[length - 1 - i] = temp;
+    }
+}
+
+// Function to convert Infix to Prefix Expression
+void infixtoprefix(char infix[], char prefix[]) {
+    struct Stack s;
+    initStack(&s);
+    int i = 0, j = 0;
+    char symbol;
+    reverseString(infix); // Reverse the infix expression
+
+    push(&s, '#');
+    for (i = 0; i < strlen(infix); i++) {
+        symbol = infix[i];
+
+        while (stackprec(s.items[s.top]) > inprec(symbol)) {
+            prefix[j++] = pop(&s);
+        }
+
+        if (stackprec(s.items[s.top]) != inprec(symbol)) {
+            push(&s, symbol);
+        } else {
+            pop(&s); // For matching '('
+        }
+    }
+
+    while (s.items[s.top] != '#') {
+        prefix[j++] = pop(&s);
+    }
+
+    prefix[j] = '\0';
+    reverseString(prefix); // Reverse the result to get the correct prefix
+}
+
+// Function to Evaluate Prefix Expression
+void prefixevaluate(char prefix[]) {
+    struct EvalStack s;
+    initEvalStack(&s);
+    int i, op1, op2, res;
+    char ch;
+
+    for (i = strlen(prefix) - 1; i >= 0; i--) {
+        ch = prefix[i];
+        if (isdigit(ch)) {
+            evalPush(&s, ch - '0');
+        } else {
+            if (isEvalEmpty(&s)) {
+                printf("Not enough operands\n");
+            } else {
+                op1 = evalPop(&s);
+                op2 = evalPop(&s);
+                switch (ch) {
+                    case '+': res = op1 + op2; break;
+                    case '-': res = op1 - op2; break;
+                    case '/': res = op1 / op2; break;
+                    case '*': res = op1 * op2; break;
+                    case '%': res = op1 % op2; break;
+                    case '^': res = op1 ^ op2; break;
+                    default: printf("Invalid Operator\n"); return;
+                }
+                evalPush(&s, res);
+            }
+        }
+    }
+    printf("Final Result = %d\n", evalPop(&s));
+}
+
+int main() {
+    char infix[MAX], prefix[MAX];
+    int choice;
+
+    printf("Menu\n1.Enter Infix Expression\n2.Convert to Prefix\n3.Evaluate Prefix\n4.Exit\n");
+    for (;;) {
+        printf("Enter Your Choice: ");
+        scanf("%d", &choice);
+        switch (choice) {
+            case 1:
+                printf("Enter the Infix Expression: ");
+                scanf("%s", infix);
+                break;
+            case 2:
+                infixtoprefix(infix, prefix);
+                printf("The Prefix Expression is: %s\n", prefix);
+                break;
+            case 3:
+                prefixevaluate(prefix);
+                break;
+            case 4:
+                printf("!! THANK YOU !!\n");
+                exit(0);
+            default:
+                printf("Invalid Choice\n");
+        }
+    }
+
+    return 0;
+}
